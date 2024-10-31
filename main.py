8a3b77fb23a11ca6bf8bbafe7b38b67b99639eb8
Lucas Federizzi <federizzi27@gmail.com>
2024-10-31 11:29:53 -0300

    add F1SENNA e F1PROST, add novo background, resolvido problema do carro amarelo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 
 pygame.init()
-tamanho = (1000,592)
+tamanho = (1061,800)
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode( tamanho )
 icone = pygame.image.load("Recursos/logo.ico")
@@ -12,17 +12,30 @@ pygame.display.set_icon(icone)
 pygame.display.set_caption("Corrida Maluca")
 branco = (255,255,255)
 preta = (0,0,0)
-fundo = pygame.image.load("Recursos/fundo.png")
-carro1 = pygame.image.load("Recursos/carro1.png")
+fundo = pygame.image.load("Recursos/interlagos4Block.png")
+carro1 = pygame.image.load("Recursos/sennaMCLaren.png")
 carro2 = pygame.image.load("Recursos/carro2.png")
-carro3 = pygame.image.load("Recursos/carro3.png")
+carro3 = pygame.image.load("Recursos/prostWilliams.png")
+
+
+distCar12 = 0
+distCar13 = 0
+
+distCar21 = 0
+distCar23 = 0
+ 
+distCar31 = 0
+distCar32 = 0  
+
+
 
 movXCar1 = 0
 movXCar2 = 0
 movXCar3 = 0
 posYCar1 = 50
-posYCar2 = 180
-posYCar3 = 110
+posYCar2 = 270
+posYCar3 = 155
+
 vitoria = pygame.mixer.Sound("Recursos/vitoria.mp3")
 vitoria.set_volume(0.5)
 pygame.mixer.music.load("Recursos/trilha.mp3")
@@ -41,8 +54,8 @@ while True:
     tela.blit(carro3, (movXCar3,posYCar3))
     
     if not acabou :
-        movXCar1 = movXCar1 + random.randint(0,1)
-        movXCar2 = movXCar2 + random.randint(0,1)
+        movXCar1 = movXCar1 + random.randint(0,10)
+        movXCar2 = movXCar2 + random.randint(0,10)
         movXCar3 = movXCar3 + random.randint(0,10)
     else:
         pygame.mixer.music.stop()
@@ -53,15 +66,15 @@ while True:
     
     if movXCar1 > 1000:
         movXCar1 = 0
-        posYCar1 = 350
+        posYCar1 = 450
         
     if movXCar2 > 1000:
         movXCar2 = 0
-        posYCar2 = 480
+        posYCar2 = 670
 
     if movXCar3 > 1000:
         movXCar3 = 0
-        posYCar3 = 400
+        posYCar3 = 555
     
     fonte = pygame.font.Font("freesansbold.ttf",60)
     textoVermelho = fonte.render("Vermelho Ganhou!", True, branco)
@@ -72,11 +85,11 @@ while True:
         tela.blit(textoVermelho, (270,70))
         acabou = True
         
-    elif posYCar2 == 480 and movXCar2 >= 900 and movXCar2 > movXCar1:
+    elif posYCar2 == 670 and movXCar2 >= 900 and movXCar2 > movXCar1 and movXCar2 > movXCar3:
         tela.blit(textoAmarelo, (270,180))
         acabou = True
     
-    elif posYCar3 == 400 and movXCar3 >= 900 and movXCar3 > movXCar1 and movXCar3 > movXCar2:
+    elif posYCar3 == 555 and movXCar3 >= 900 and movXCar3 > movXCar1 and movXCar3 > movXCar2:
         tela.blit(textoAzul, (270,480))
         acabou = True
 
